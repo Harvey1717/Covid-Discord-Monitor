0b678e977f6905014fee34b1e554d33787c196eb
Harvey Mac <harvey.woodall@me.com>
2020-03-21 17:58:03 +0000

    Improved logging

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ function getInfectedCount() {
     .json()
     .then(response => {
       const countryData = response.find(countryData => countryData.country === 'UK');
-      console.log(countryData);
       if (_.isEqual(countryDataTemp.countryData, countryData)) {
+        console.log(countryData);
         sendHook(countryData);
       } else {
         console.log('No Change');
